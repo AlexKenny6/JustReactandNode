1a4fe46bf885e613a87fec8084c1efa3aa1fe824
AlexKenny6 <alexkenny27@gmail.com>
2022-02-04 14:35:08 -0800

    Saving point for 2/4/22

--- learning-all-about-react/src/components/ExpenseItem.js
+++ learning-all-about-react/src/components/ExpenseItem.js
@@ -2,11 +2,17 @@ import './ExpenseItem.css';
 
 function ExpenseItem(expenseProps) {
 
+    const month = expenseProps.date.toLocaleString('en-us', {month: 'long'});
+    const year = expenseProps.date.toLocaleString('en-us', {year: '2-digit'});
+    const day = expenseProps.date.getFullYear();
+
     return (
 
         <div className="expense-item">
             <div>
-                {expenseProps.date.toISOString()}
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
             </div>
             <div className="expense-item__description">
                 <h2>{expenseProps.title}</h2>
